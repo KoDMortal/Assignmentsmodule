f680397cd4c83ac325c2013e30c875ee573e10ba
KoDMortal <kodmortal999@gmail.com>
2023-08-31 18:50:43 +0300

    Create module1-3.py

---
+++ module1-3.py
@@ -0,0 +1,7 @@
+"""Module 1"""
+"""Exersice 1
+firstname = input("Enter your firstname please")
+secondname = input("Enter your secondname please")
+print("Hello, " + firstname + " " + secondname + "!")"""
+"""Exersice 2"""
+
